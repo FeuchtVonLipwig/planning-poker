63e0425da707a7013819147aeb4541fa49792e90
Daniel Feist <daniel-feist@web.de>
2025-12-12 16:15:33 +0100

    Change websocket address to work online

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,4 +1,11 @@
 // frontend/src/socket.ts
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000");
+// export const socket = io("http://localhost:3000");
+
+
+const socketUrl = window.location.origin;
+
+export const socket = io(socketUrl, {
+  transports: ["websocket"], // helps on hosted environments
+});
