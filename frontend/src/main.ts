d86b13ae2d4a1ffa74469cb8b134a1c3ef1f263a
Daniel <126024246+FeuchtVonLipwig@users.noreply.github.com>
2026-01-09 17:58:18 +0100

    Update main.ts
    Remove second websocket being opened in main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,10 @@
 import './assets/main.css'
+import './style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './style.css'  
-
-import { io } from "socket.io-client";
-
-export const socket = io("http://localhost:3000");
 
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
